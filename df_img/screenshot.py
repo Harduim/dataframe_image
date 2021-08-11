de51cf206b03237917ed6df48f78fbd786148589
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2021-08-11 11:03:16 -0300

    Removed unused functionality

--- df_img/screenshot.py
+++ df_img/screenshot.py
@@ -12,7 +12,7 @@ from matplotlib import image as mimage
 
 def get_system():
     system = platform.system().lower()
-    if system in ['darwin', 'linux', 'windows']:
+    if system in ["darwin", "linux", "windows"]:
         return system
     else:
         raise OSError(f"Unsupported OS - {system}")
@@ -32,7 +32,8 @@ def get_chrome_path(chrome_path=None):
             if Path(path).exists():
                 return path
         raise OSError("Chrome executable not able to be found on your machine")
-    elif system == "linux":
+
+    if system == "linux":
         paths = [
             None,
             "/usr/local/sbin",
@@ -50,8 +51,10 @@ def get_chrome_path(chrome_path=None):
                 if chrome_path:
                     return chrome_path
         raise OSError("Chrome executable not able to be found on your machine")
-    elif system == "windows":
+
+    if system == "windows":
         import winreg
+
         locs = [
             r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe",
             r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\brave.exe",
@@ -65,9 +68,16 @@ def get_chrome_path(chrome_path=None):
 
 
 class Screenshot:
-
-    def __init__(self, center_df=True, max_rows=None, max_cols=None, chrome_path=None, 
-                 fontsize=18, encode_base64=True, limit_crop=True):
+    def __init__(
+        self,
+        center_df=True,
+        max_rows=None,
+        max_cols=None,
+        chrome_path=None,
+        fontsize=18,
+        encode_base64=True,
+        limit_crop=True,
+    ):
         self.center_df = center_df
         self.max_rows = max_rows
         self.max_cols = max_cols
@@ -83,7 +93,7 @@ class Screenshot:
         css_file = mod_dir / "static" / "style.css"
         with open(css_file) as f:
             css = "<style>" + f.read() + "</style>"
-        justify = 'center' if self.center_df else 'left'
+        justify = "center" if self.center_df else "left"
         css = css.format(fontsize=fontsize, justify=justify)
         return css
 
@@ -94,25 +104,17 @@ class Screenshot:
         with open(temp_html, "w") as f:
             f.write(self.html)
 
-        with open(temp_img, "wb") as f:        
-            args = [
-                "--enable-logging",
-                "--disable-gpu",
-                "--headless"
-                ]
+        with open(temp_img, "wb") as f:
+            args = ["--enable-logging", "--disable-gpu", "--headless"]
 
             if self.ss_width and self.ss_height:
                 args.append(f"--window-size={self.ss_width},{self.ss_height}")
 
-            args += [
-                "--hide-scrollbars",
-                f"--screenshot={str(temp_img)}",
-                str(temp_html)
-                ]
+            args += ["--hide-scrollbars", f"--screenshot={str(temp_img)}", str(temp_html)]
 
             subprocess.run(executable=self.chrome_path, args=args)
 
-        with open(temp_img, 'rb') as f:
+        with open(temp_img, "rb") as f:
             img_bytes = f.read()
 
         buffer = io.BytesIO(img_bytes)
@@ -133,7 +135,7 @@ class Screenshot:
         if all_white_horiz[-30:].sum() != 30:
             self.ss_height = int(self.ss_height * 1.5)
             enlarge = True
-            
+
         if enlarge:
             return self.take_screenshot()
 
@@ -144,7 +146,7 @@ class Screenshot:
         left = diff_vert.argmax()
         right = -diff_vert[::-1].argmax()
         if self.limit_crop:
-            max_crop = int(img.shape[1] * .15)
+            max_crop = int(img.shape[1] * 0.15)
             left = min(left, max_crop)
             right = max(right, -max_crop)
 
@@ -154,7 +156,7 @@ class Screenshot:
         new_img = img[top:bottom, left:right]
         return new_img
 
-    def finalize_image(self, img):        
+    def finalize_image(self, img):
         buffer = io.BytesIO()
         mimage.imsave(buffer, img)
         img_str = buffer.getvalue()
@@ -171,39 +173,12 @@ class Screenshot:
     def repr_png_wrapper(self):
         from pandas.io.formats.style import Styler
         ss = self
+
         def _repr_png_(self):
             if isinstance(self, Styler):
-                html = '<div>' + self.render() + '</div>'
+                html = "<div>" + self.render() + "</div>"
             else:
                 html = self.to_html(max_rows=ss.max_rows, max_cols=ss.max_cols, notebook=True)
             return ss.run(html)
+
         return _repr_png_
-
-
-def make_repr_png(center_df=True, max_rows=30, max_cols=10, chrome_path=None):
-    """
-    Used to create a _repr_png_ method for DataFrames and Styler objects
-    so that nbconvert can use it to create images directly when 
-    executing the notebook before conversion to pdf/markdown.
-
-    Parameters
-    ----------
-    center_df : bool, default True
-        Choose whether to center the DataFrames or not in the image. By 
-        default, this is True, though in Jupyter Notebooks, they are 
-        left-aligned. Use False to make left-aligned.
-
-    max_rows : int, default 30
-        Maximum number of rows to output from DataFrame. This is forwarded to 
-        the `to_html` DataFrame method.
-
-    max_cols : int, default 10
-        Maximum number of columns to output from DataFrame. This is forwarded 
-        to the `to_html` DataFrame method.
-
-    chrome_path : str, default None
-        Path to your machine's chrome executable. When `None`, it is 
-        automatically found. Use this when chrome is not automatically found.
-    """
-    ss = Screenshot(center_df, max_rows, max_cols, chrome_path)
-    return ss.repr_png_wrapper()
