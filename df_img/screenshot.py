9a25eaab8b6ea98cddcd1e8c34b406a858f6d2d4
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2023-03-23 14:57:35 -0300

    Chore: upstream patch

--- df_img/screenshot.py
+++ df_img/screenshot.py
@@ -1,13 +1,15 @@
+import base64
+import io
 import platform
 import shutil
 import subprocess
-import base64
-import io
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import numpy as np
 from matplotlib import image as mimage
+
+from .pd_html import styler2html
 
 
 def get_system():
@@ -32,8 +34,7 @@ def get_chrome_path(chrome_path=None):
             if Path(path).exists():
                 return path
         raise OSError("Chrome executable not able to be found on your machine")
-
-    if system == "linux":
+    elif system == "linux":
         paths = [
             None,
             "/usr/local/sbin",
@@ -44,19 +45,25 @@ def get_chrome_path(chrome_path=None):
             "/bin",
             "/opt/google/chrome",
         ]
-        commands = ["google-chrome", "chrome", "chromium", "chromium-browser", "brave-browser"]
+        commands = [
+            "google-chrome",
+            "chrome",
+            "chromium",
+            "chromium-browser",
+            "brave-browser",
+        ]
         for path in paths:
             for cmd in commands:
                 chrome_path = shutil.which(cmd, path=path)
                 if chrome_path:
                     return chrome_path
         raise OSError("Chrome executable not able to be found on your machine")
-
-    if system == "windows":
+    elif system == "windows":
         import winreg
 
         locs = [
             r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe",
+            r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe",
             r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\brave.exe",
         ]
         for loc in locs:
@@ -77,16 +84,16 @@ class Screenshot:
         fontsize=18,
         encode_base64=True,
         limit_crop=True,
+        device_scale_factor=1,
     ):
         self.center_df = center_df
         self.max_rows = max_rows
         self.max_cols = max_cols
-        self.ss_width = 1400
-        self.ss_height = 900
         self.chrome_path = get_chrome_path(chrome_path)
         self.css = self.get_css(fontsize)
         self.encode_base64 = encode_base64
         self.limit_crop = limit_crop
+        self.device_scale_factor = device_scale_factor
 
     def get_css(self, fontsize):
         mod_dir = Path(__file__).resolve().parent
@@ -97,48 +104,54 @@ class Screenshot:
         css = css.format(fontsize=fontsize, justify=justify)
         return css
 
-    def take_screenshot(self):
+    def take_screenshot(self, ss_width=1400, ss_height=900):
         temp_dir = TemporaryDirectory()
         temp_html = Path(temp_dir.name) / "temp.html"
         temp_img = Path(temp_dir.name) / "temp.png"
-        with open(temp_html, "w") as f:
+        with open(temp_html, "w", encoding="utf-8") as f:
             f.write(self.html)
 
-        with open(temp_img, "wb") as f:
-            args = ["--enable-logging", "--disable-gpu", "--headless", "--no-sandbox"]
+        args = [
+            "--enable-logging",
+            "--disable-gpu",
+            "--headless=new",
+            "--no-sandbox",
+            "--crash-dumps-dir=/tmp",
+            f"--force-device-scale-factor={self.device_scale_factor}",
+        ]
 
-            if self.ss_width and self.ss_height:
-                args.append(f"--window-size={self.ss_width},{self.ss_height}")
+        if ss_width and ss_height:
+            args.append(f"--window-size={ss_width},{ss_height}")
 
-            args += [
-                "--hide-scrollbars", f"--screenshot={str(temp_img)}", str(temp_html)]
+        args += [
+            "--hide-scrollbars",
+            f"--screenshot={str(temp_img)}",
+            str(temp_html),
+        ]
 
-            subprocess.run(executable=self.chrome_path, args=args)
+        self.generate_image_from_html(args)
 
         with open(temp_img, "rb") as f:
             img_bytes = f.read()
 
         buffer = io.BytesIO(img_bytes)
         img = mimage.imread(buffer)
-        return self.possibly_enlarge(img)
+        return self.possibly_enlarge(img, ss_width, ss_height)
 
-    def possibly_enlarge(self, img):
-        enlarge = False
+    def generate_image_from_html(self, args):
+        subprocess.run(executable=self.chrome_path, args=args, check=True)
+
+    def possibly_enlarge(self, img, ss_width, ss_height):
         img2d = img.mean(axis=2) == 1
 
         all_white_vert = img2d.all(axis=0)
         # must be all white for 30 pixels in a row to trigger stop
-        if all_white_vert[-30:].sum() != 30:
-            self.ss_width = int(self.ss_width * 1.5)
-            enlarge = True
+        if all_white_vert[-5:].sum() != 5:
+            ss_width = int(ss_width * 1.5)
 
         all_white_horiz = img2d.all(axis=1)
-        if all_white_horiz[-30:].sum() != 30:
-            self.ss_height = int(self.ss_height * 1.5)
-            enlarge = True
-
-        if enlarge:
-            return self.take_screenshot()
+        if all_white_horiz[-5:].sum() != 5:
+            ss_height = int(ss_height * 1.5)
 
         return self.crop(img, all_white_vert, all_white_horiz)
 
@@ -146,14 +159,19 @@ class Screenshot:
         diff_vert = np.diff(all_white_vert)
         left = diff_vert.argmax()
         right = -diff_vert[::-1].argmax()
+        if right == 0:
+            right = None
         if self.limit_crop:
             max_crop = int(img.shape[1] * 0.15)
             left = min(left, max_crop)
-            right = max(right, -max_crop)
+            if right is not None:
+                right = max(right, -max_crop)
 
         diff_horiz = np.diff(all_white_horiz)
         top = diff_horiz.argmax()
         bottom = -diff_horiz[::-1].argmax()
+        if bottom == 0:
+            bottom = None
         new_img = img[top:bottom, left:right]
         return new_img
 
@@ -173,13 +191,43 @@ class Screenshot:
 
     def repr_png_wrapper(self):
         from pandas.io.formats.style import Styler
+
         ss = self
 
         def _repr_png_(self):
             if isinstance(self, Styler):
-                html = "<div>" + self.render() + "</div>"
+                html = styler2html(self)
             else:
                 html = self.to_html(max_rows=ss.max_rows, max_cols=ss.max_cols, notebook=True)
             return ss.run(html)
 
         return _repr_png_
+
+
+def make_repr_png(center_df=True, max_rows=30, max_cols=10, chrome_path=None):
+    """
+    Used to create a _repr_png_ method for DataFrames and Styler objects
+    so that nbconvert can use it to create images directly when
+    executing the notebook before conversion to pdf/markdown.
+
+    Parameters
+    ----------
+    center_df : bool, default True
+        Choose whether to center the DataFrames or not in the image. By
+        default, this is True, though in Jupyter Notebooks, they are
+        left-aligned. Use False to make left-aligned.
+
+    max_rows : int, default 30
+        Maximum number of rows to output from DataFrame. This is forwarded to
+        the `to_html` DataFrame method.
+
+    max_cols : int, default 10
+        Maximum number of columns to output from DataFrame. This is forwarded
+        to the `to_html` DataFrame method.
+
+    chrome_path : str, default None
+        Path to your machine's chrome executable. When `None`, it is
+        automatically found. Use this when chrome is not automatically found.
+    """
+    ss = Screenshot(center_df, max_rows, max_cols, chrome_path)
+    return ss.repr_png_wrapper()
